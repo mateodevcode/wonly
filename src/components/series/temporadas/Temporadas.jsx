ba438fc398ef91b27a753f712901a001ee7504bd
Mateo Lizcano <149150878+mateodevcode@users.noreply.github.com>
2024-10-01 19:02:49 +0200

    Se actualizo el componente Temporadas.jsx

--- src/components/series/temporadas/Temporadas.jsx
+++ src/components/series/temporadas/Temporadas.jsx
@@ -34,7 +34,7 @@ const Temporadas = () => {
         <h2 className="uppercase text-center font-bold lg:text-5xl sm:text-3xl dark:text-white mb-10">
           {datosSeries.titulo}
         </h2>
-        <div className="lg:w-10/12 md:w-10/12 sm:w-11/12 grid lg:grid-cols-5 sm:grid-cols-1 gap-4">
+        <div className="lg:w-10/12 md:w-10/12 sm:w-11/12 grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 gap-4 place-items-center">
           {temporada.map((temp, index) => (
             <CardTemporadas
               key={index}
