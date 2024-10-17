81aad1de0b327abaf6f60d5d884d3b0ae31f3b18
Mateo Lizcano <149150878+mateodevcode@users.noreply.github.com>
2024-10-18 01:17:37 +0200

    Se mejoro el spinner global

--- src/components/spinner/SpinnerGlobal.jsx
+++ src/components/spinner/SpinnerGlobal.jsx
@@ -1,15 +1,24 @@
 import { Spinner } from "@chakra-ui/react";
+import Logo from "../navbar/Logo";
 
 const SpinnerGlobal = () => {
   return (
-    <Spinner
-      thickness="4px"
-      speed="1.65s"
-      emptyColor="gray.100"
-      color="green.500"
-      size="xl"
-      className="my-40"
-    />
+    <div className="my-40 flex flex-col justify-center items-center">
+      <Spinner
+        thickness="4px"
+        speed="1.65s"
+        emptyColor="gray.100"
+        color="green.500"
+        size="xl"
+        className=""
+      />
+      <div>
+        <h3 className="text-center text-white font-mono text-xl my-5">
+          Cargando...
+        </h3>
+        <Logo />
+      </div>
+    </div>
   );
 };
 
