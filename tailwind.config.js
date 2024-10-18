b3bf87a9647d30d3127f9435deed21347def72d5
Mateo Lizcano <149150878+mateodevcode@users.noreply.github.com>
2024-10-18 02:54:01 +0200

    Se cambio el tamaño de las fuentes en el archivo tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,7 +23,7 @@ module.exports = {
       'lg': '1024px',
       // => @media (min-width: 1024px) { ... }
 
-      'xl': '1280px',
+      'xl': '1540px',
       // => @media (min-width: 1280px) { ... }
     }
   },
