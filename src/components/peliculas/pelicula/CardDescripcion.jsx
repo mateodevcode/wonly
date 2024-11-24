a80ac48ae8e58bdad2575bdf6c9390b4e1fdca52
Mateo Lizcano <149150878+mateodevcode@users.noreply.github.com>
2024-11-24 07:20:49 +0100

    se cambio el tamaño de la descripcion de la pelicula

--- src/components/peliculas/pelicula/CardDescripcion.jsx
+++ src/components/peliculas/pelicula/CardDescripcion.jsx
@@ -2,12 +2,12 @@ import BotonHora from "./BotonHora";
 
 const CardDescripcion = ({ titulo, duracion, descripcion, temporada, episodio }) => {
   return (
-    <div className="xl:w-[1440px] lg:w-[950px] md:w-[800px] sm:w-full flex flex-col justify-center items-start lg:mb-20 md:mb-20 sm:mb-5 lg:mt-2 md:mt-2 sm:mt-0">
-      <div className="font-bold lg:text-2xl md:text-2xl sm:text-sm mt-2 dark:text-white flex flex-row justify-between items-center w-full px-2">
+    <div className="xl:w-[1440px] lg:w-[950px] md:w-full sm:w-full flex flex-col justify-center items-start lg:mb-20 md:mb-20 sm:mb-5 lg:mt-2 md:mt-2 sm:mt-0 px-5 bg-slate-500">
+      <div className="font-bold lg:text-2xl md:text-2xl sm:text-sm mt-2 dark:text-white flex flex-row justify-between items-center w-full">
         {temporada ? temporada + " " + episodio + " - " : null} { titulo}
         <BotonHora hora={duracion} />
       </div>
-      <p className="lg:text-base md:text-base sm:text-xs dark:text-white mt-2 px-2">
+      <p className="lg:text-base md:text-base sm:text-xs dark:text-white mt-2">
         {descripcion}
       </p>
     </div>
