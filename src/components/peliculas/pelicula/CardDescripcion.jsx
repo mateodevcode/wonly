6428a85a7643200255bff720fa08457211ac8743
Mateo Lizcano <149150878+mateodevcode@users.noreply.github.com>
2024-11-24 07:25:31 +0100

    Se ha modificado el archivo CardDescripcion.jsx

--- src/components/peliculas/pelicula/CardDescripcion.jsx
+++ src/components/peliculas/pelicula/CardDescripcion.jsx
@@ -2,7 +2,7 @@ import BotonHora from "./BotonHora";
 
 const CardDescripcion = ({ titulo, duracion, descripcion, temporada, episodio }) => {
   return (
-    <div className="xl:w-[1440px] lg:w-[950px] md:w-full sm:w-full flex flex-col justify-center items-start lg:mb-20 md:mb-20 sm:mb-5 lg:mt-2 md:mt-2 sm:mt-0 px-5 bg-slate-500">
+    <div className="xl:w-[1440px] lg:w-[950px] md:w-full sm:w-full flex flex-col justify-center items-start lg:mb-20 md:mb-20 sm:mb-5 lg:mt-2 md:mt-2 sm:mt-0 px-5">
       <div className="font-bold lg:text-2xl md:text-2xl sm:text-sm mt-2 dark:text-white flex flex-row justify-between items-center w-full">
         {temporada ? temporada + " " + episodio + " - " : null} { titulo}
         <BotonHora hora={duracion} />
